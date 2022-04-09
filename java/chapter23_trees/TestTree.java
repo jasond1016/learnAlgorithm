216d2ad129695744a9a2b7145e28768e5520ce26
atdt <jasond1016@gmail.com>
2022-04-09 21:53:36 +0800

    leetcode 94. Binary Tree Inorder Traversal

--- java/chapter23_trees/TestTree.java
+++ java/chapter23_trees/TestTree.java
@@ -26,6 +26,7 @@ public class TestTree {
                 )
         );
         System.out.println(levelOrder(root));
+        System.out.println(inorderTraversal(root));
 
     }
 
@@ -55,6 +56,27 @@ public class TestTree {
             }
             result.add(innerList);
         }
+        return result;
+    }
+
+    // leetcode 94. Binary Tree Inorder Traversal
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<Integer>();
+        }
+
+        List<Integer> result = new ArrayList<Integer>();
+
+        if (root.left != null) {
+            result.addAll(inorderTraversal(root.left));
+        }
+
+        result.add(root.val);
+
+        if (root.right != null) {
+            result.addAll(inorderTraversal(root.right));
+        }
+
         return result;
     }
 }

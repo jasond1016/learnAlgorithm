fa77ca646a39708cfa019a492628d6b700af4a45
atdt <jasond1016@gmail.com>
2022-04-15 15:47:37 +0800

    字符串匹配-RK算法

--- java/chapter32_string_match/StringUtil.java
+++ java/chapter32_string_match/StringUtil.java
@@ -1,19 +1,49 @@
 package chapter32_string_match;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StringUtil {
     public static int bfFind(String a, String b) {
         int n = a.length();
         int m = b.length();
 
         for (int i = 0; i < n - m + 1; i++) {
+            int c = 0;
             for (int j = 0; j < m; j++) {
-                if (a.charAt(i + j) != b.charAt(j)) {
+                if (a.charAt(i + j) == b.charAt(j)) {
+                    c++;
+                } else {
                     break;
                 }
-                return i;
+                if (c == m) {
+                    return i;
+                }
             }
         }
         return -1;
+    }
+
+    public static int rkFind(String a, String b) {
+        int n = a.length();
+        int m = b.length();
+        if (n == 0 || m == 0) {
+            return -1;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n - m + 1; i++) {
+            int hashCode = 0;
+            for (int j = 0; j < m; j++) {
+                hashCode = 26 * hashCode + (a.charAt(i + j) - 'a');
+            }
+            map.put(hashCode, i);
+        }
+
+        int hashCode = 0;
+        for (int j = 0; j < m; j++) {
+            hashCode = 26 * hashCode + (b.charAt(j) - 'a');
+        }
+        return map.getOrDefault(hashCode, -1);
     }
 
 }
--- java/chapter32_string_match/TestStringUtil.java
+++ java/chapter32_string_match/TestStringUtil.java
@@ -2,15 +2,29 @@ package chapter32_string_match;
 
 public class TestStringUtil {
     public static void main(String[] args) {
-        System.out.println(StringUtil.bfFind("1234567", "13"));
-        System.out.println(StringUtil.bfFind("1234567", "1234567"));
-        System.out.println(StringUtil.bfFind("1234567", "123456"));
-        System.out.println(StringUtil.bfFind("1234567", "234567"));
-        System.out.println(StringUtil.bfFind("1234567", "345"));
-        System.out.println(StringUtil.bfFind("1234567", "7"));
-        System.out.println(StringUtil.bfFind("1234567", ""));
-        System.out.println(StringUtil.bfFind("", "1"));
+        System.out.println(StringUtil.bfFind("abcdefg", "ac"));
+        System.out.println(StringUtil.bfFind("abcdefg", "abcdefg"));
+        System.out.println(StringUtil.bfFind("abcdefg", "abcdef"));
+        System.out.println(StringUtil.bfFind("abcdefg", "bcdefg"));
+        System.out.println(StringUtil.bfFind("abcdefg", "cde"));
+        System.out.println(StringUtil.bfFind("abcdefg", "g"));
+        System.out.println(StringUtil.bfFind("abcdefg", ""));
+        System.out.println(StringUtil.bfFind("", "a"));
         System.out.println(StringUtil.bfFind("", ""));
+        System.out.println(StringUtil.rkFind("c", "c"));
+
+        System.out.println("----");
+
+        System.out.println(StringUtil.rkFind("abcdefg", "ac"));
+        System.out.println(StringUtil.rkFind("abcdefg", "abcdefg"));
+        System.out.println(StringUtil.rkFind("abcdefg", "abcdef"));
+        System.out.println(StringUtil.rkFind("abcdefg", "bcdefg"));
+        System.out.println(StringUtil.rkFind("abcdefg", "cde"));
+        System.out.println(StringUtil.rkFind("abcdefg", "g"));
+        System.out.println(StringUtil.rkFind("abcdefg", ""));
+        System.out.println(StringUtil.rkFind("", "a"));
+        System.out.println(StringUtil.rkFind("", ""));
+        System.out.println(StringUtil.rkFind("c", "c"));
     }
 
 }

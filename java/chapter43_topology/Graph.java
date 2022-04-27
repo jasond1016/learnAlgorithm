cf84aa41a28da3ea26625c4805f57225ff05f5ae
atdt <jasond1016@gmail.com>
2022-04-27 18:17:00 +0800

    拓扑排序-DFS算法

--- java/chapter43_topology/Graph.java
+++ java/chapter43_topology/Graph.java
@@ -6,17 +6,19 @@ import java.util.Queue;
 public class Graph {
     int n;
     LinkedList<Integer>[] adj;
-    
+
     public static void main(String[] args) {
         Graph graph = new Graph(5);
         graph.addEdge(0, 1);
         graph.addEdge(0, 2);
         graph.addEdge(2, 3);
         graph.addEdge(1, 4);
-        
+
         graph.topoSortByKahn();
+        System.out.println();
+        graph.topoSortByDFS();
     }
-    
+
     public Graph(int n) {
         this.n = n;
         adj = new LinkedList[n];
@@ -43,7 +45,7 @@ public class Graph {
                 queue.add(i);
             }
         }
-        
+
         while (!queue.isEmpty()) {
             int i = queue.remove();
             System.out.print("->" + i);
@@ -56,6 +58,43 @@ public class Graph {
             }
         }
     }
-    
+
+    public void topoSortByDFS() {
+        LinkedList<Integer>[] inverseAdj = new LinkedList[n];
+        for (int i = 0; i < inverseAdj.length; i++) {
+            inverseAdj[i] = new LinkedList<>();
+        }
+
+        // 逆邻接表
+        for (int i = 0; i < adj.length; i++) {
+            for (int j = 0; j < adj[i].size(); j++) {
+                int w = adj[i].get(j);
+                inverseAdj[w].add(i);
+            }
+        }
+
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(i, inverseAdj, visited);
+            }
+        }
+
+    }
+
+    private void dfs(int i, LinkedList<Integer>[] inverseAdj, boolean[] visited) {
+        for (int j = 0; j < inverseAdj[i].size(); j++) {
+            int w = inverseAdj[i].get(j);
+            if (!visited[w]) {
+                visited[w] = true;
+                dfs(w, inverseAdj, visited);
+            } else {
+                continue;
+            }
+        }
+        System.out.print("->" + i);
+    }
+
 
 }

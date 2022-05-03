0b59b989872d70e1147524c28f6e3eb90a5801ab
atdt <jasond1016@gmail.com>
2022-05-03 22:48:04 +0800

    LeetCode 20. Valid Parentheses

---
+++ java/leetcode/p0020_valid_parentheses/Solution.java
@@ -0,0 +1,40 @@
+package leetcode.p0020_valid_parentheses;
+
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "()";
+        System.out.println(solution.isValid(s));
+    }
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(c);
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                    break;
+            }
+        }
+        return stack.isEmpty();
+    }
+}

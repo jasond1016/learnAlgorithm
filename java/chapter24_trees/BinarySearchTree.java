e3f00178cd291dc8a3855a4353b8f33314833f45
atdt <jasond1016@gmail.com>
2022-05-13 21:27:37 +0800

    refactor: remove unusable file

--- java/chapter24_trees/BinarySearchTree.java
+++ java/chapter24_trees/BinarySearchTree.java
@@ -181,3 +181,22 @@ public class BinarySearchTree {
         return result;
     }
 }
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

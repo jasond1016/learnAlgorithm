d48f4f78d082e49b869a9356ea87a1f1a9bbeae6
atdt <jasond1016@gmail.com>
2022-05-18 18:42:13 +0800

    LeetCode 695. Max Area of Island

---
+++ java/leetcode/p0695_max_area_of_island/Solution.java
@@ -0,0 +1,43 @@
+package leetcode.p0695_max_area_of_island;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] grid = new int[][]{{0,0,1,0,0,0,0,1,0,0,0,0,0},{0,0,0,0,0,0,0,1,1,1,0,0,0},{0,1,1,0,1,0,0,0,0,0,0,0,0},{0,1,0,0,1,1,0,0,1,0,1,0,0},{0,1,0,0,1,1,0,0,1,1,1,0,0},{0,0,0,0,0,0,0,0,0,0,1,0,0},{0,0,0,0,0,0,0,1,1,1,0,0,0},{0,0,0,0,0,0,0,1,1,0,0,0,0}};
+        System.out.println(solution.maxAreaOfIsland(grid));
+    }
+    public int maxAreaOfIsland(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    res = Math.max(res, dfs(grid, i, j));
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if (i < 0 || j < 0 || i >= m || j >= n) {
+            return 0;
+        }
+
+        if (grid[i][j] == 0) {
+            return 0;
+        }
+
+        grid[i][j] = 0;
+        return dfs(grid, i - 1, j)
+                + dfs(grid, i + 1, j)
+                + dfs(grid, i, j - 1)
+                + dfs(grid, i, j + 1) + 1;
+    }
+}

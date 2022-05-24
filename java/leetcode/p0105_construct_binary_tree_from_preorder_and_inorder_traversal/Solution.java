f919fab3b2c2b0bd76b7ec4bbff78808c93f791d
atdt <jasond1016@gmail.com>
2022-05-24 14:43:22 +0800

    LeetCode 105. Construct Binary Tree from Preorder and Inorder Traversal

---
+++ java/leetcode/p0105_construct_binary_tree_from_preorder_and_inorder_traversal/Solution.java
@@ -0,0 +1,64 @@
+package leetcode.p0105_construct_binary_tree_from_preorder_and_inorder_traversal;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Jason
+ * @date 2022/5/24
+ */
+public class Solution {
+    private Map<Integer, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] preorder = new int[]{3, 9, 20, 15, 7};
+        int[] inorder = new int[]{9, 3, 15, 20, 7};
+        TreeNode res = solution.buildTree(preorder, inorder);
+        System.out.println(res);
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return build(preorder, 0, preorder.length - 1,
+                inorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode build(int[] preorder, int preStart, int preEnd,
+                           int[] inorder, int inStart, int inEnd) {
+        if (preStart > preEnd) {
+            return null;
+        }
+        int rootVal = preorder[preStart];
+        int index = map.getOrDefault(rootVal, -1);
+
+        TreeNode root = new TreeNode(rootVal);
+        int leftSize = index - inStart;
+        root.left = build(preorder, preStart + 1, preStart + leftSize,
+                inorder, inStart, index - 1);
+        root.right = build(preorder, preStart + leftSize + 1, preEnd,
+                inorder, index + 1, inEnd);
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

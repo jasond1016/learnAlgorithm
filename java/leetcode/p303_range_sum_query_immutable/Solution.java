7e06bc915cc8bf5271c6789442adefacf45fefcf
atdt <jasond1016@gmail.com>
2022-05-28 17:04:08 +0800

    LeetCode 303. Range Sum Query - Immutable

---
+++ java/leetcode/p303_range_sum_query_immutable/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.p303_range_sum_query_immutable;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2, 0, 3, -5, 2, -1};
+        NumArray obj = new NumArray(nums);
+        System.out.println(obj.sumRange(0, 2));
+        System.out.println(obj.sumRange(2, 5));
+        System.out.println(obj.sumRange(0, 5));
+    }
+}
+
+class NumArray {
+
+    private int[] preSum;
+    public NumArray(int[] nums) {
+        preSum = new int[nums.length + 1];
+        for (int i = 1; i < preSum.length; i++) {
+            preSum[i] = preSum[i - 1] + nums[i - 1];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return preSum[right + 1] - preSum[left];
+    }
+}

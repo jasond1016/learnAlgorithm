7d097907805da25c3696936c70b21d11f8195fa2
atdt <jasond1016@gmail.com>
2022-06-01 18:21:53 +0800

    LeetCode 1480. Running Sum of 1d Array(Space O(1))

--- java/leetcode/p1480_running_sum_of_1d_array/Solution.java
+++ java/leetcode/p1480_running_sum_of_1d_array/Solution.java
@@ -14,11 +14,9 @@ public class Solution {
     }
 
     public int[] runningSum(int[] nums) {
-        int[] result = new int[nums.length];
-        result[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            result[i] = result[i - 1] + nums[i];
+            nums[i] = nums[i - 1] + nums[i];
         }
-        return result;
+        return nums;
     }
 }

231ac74c71ae840ebef268a8bc2c1f5dfd63cb91
atdt <jasond1016@gmail.com>
2022-06-13 19:12:44 +0800

    LeetCode 222. Count Complete Tree Nodes

---
+++ java/leetcode/p0222_count_complete_tree_nodes/Solution.java
@@ -0,0 +1,47 @@
+package leetcode.p0222_count_complete_tree_nodes;
+
+public class Solution {
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode root = new TreeNode(1,
+                new TreeNode(2,
+                        new TreeNode(4),
+                        new TreeNode(5)),
+                new TreeNode(3,
+                        new TreeNode(6),
+                        null));
+        System.out.println(solution.countNodes(root));
+    }
+    public int countNodes(TreeNode root) {
+        return traverse(root);
+    }
+
+    private int traverse(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = traverse(root.left);
+        int right = traverse(root.right);
+        return 1 + left + right;
+    }
+}

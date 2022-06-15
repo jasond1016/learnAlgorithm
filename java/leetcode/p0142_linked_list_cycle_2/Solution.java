c53f8ac5da5ecfde9d81df553da5a94974837f96
atdt <jasond1016@gmail.com>
2022-06-15 20:34:31 +0800

    LeetCode 142. Linked List Cycle II

---
+++ java/leetcode/p0142_linked_list_cycle_2/Solution.java
@@ -0,0 +1,55 @@
+package leetcode.p0142_linked_list_cycle_2;
+
+public class Solution {
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode head = new ListNode(3);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(0);
+        ListNode fourth = new ListNode(-4);
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = second;
+        ListNode res = solution.detectCycle(head);
+        System.out.println(res);
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}

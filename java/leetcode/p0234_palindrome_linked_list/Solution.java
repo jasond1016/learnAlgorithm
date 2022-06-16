d4e230d3651ff995d89bc6d60f966bb783e1cf2a
atdt <jasond1016@gmail.com>
2022-06-16 20:41:45 +0800

    LeetCode 234. Palindrome Linked List

---
+++ java/leetcode/p0234_palindrome_linked_list/Solution.java
@@ -0,0 +1,46 @@
+package leetcode.p0234_palindrome_linked_list;
+
+public class Solution {
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode head = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(2,
+                                new ListNode(1))));
+        System.out.println(solution.isPalindrome(head));
+        
+    }
+
+    ListNode left;
+    public boolean isPalindrome(ListNode head) {
+        left = head;
+        return traverse(head);
+    }
+
+    private boolean traverse(ListNode right) {
+        if (right == null) {
+            return true;
+        }
+        boolean res = traverse(right.next);
+        res = res && (left.val == right.val);
+        left = left.next;
+        return res;
+    }
+}

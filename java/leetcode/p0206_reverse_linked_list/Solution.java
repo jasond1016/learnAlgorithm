995fce13e58ef1c375e999d6fd3cfa497e2f25d6
atdt <jasond1016@gmail.com>
2022-06-16 19:03:06 +0800

    LeetCode 206. Reverse Linked List

---
+++ java/leetcode/p0206_reverse_linked_list/Solution.java
@@ -0,0 +1,41 @@
+package leetcode.p0206_reverse_linked_list;
+
+public class Solution {
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode head = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(3,
+                                new ListNode(4,
+                                        new ListNode(5, null)))));
+        ListNode res = solution.reverseList(head);
+        System.out.println(res);
+    }
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode last = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+}

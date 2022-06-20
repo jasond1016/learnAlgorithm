5ce90dd96070d0e7aff09e03ad80a1d165cb99b4
atdt <jasond1016@gmail.com>
2022-06-20 18:49:43 +0800

    LeetCode 5. Longest Palindromic Substring(fast-slow pointer solution)

--- java/leetcode/p0005_longest_palindromic_substring/Solution.java
+++ java/leetcode/p0005_longest_palindromic_substring/Solution.java
@@ -12,20 +12,39 @@ public class Solution {
         System.out.println(solution.longestPalindrome("azwdzwmwcqzgcobeeiphemqbjtxzwkhiqpbrprocbppbxrnsxnwgikiaqutwpftbiinlnpyqstkiqzbggcsdzzjbrkfmhgtnbujzszxsycmvipjtktpebaafycngqasbbhxaeawwmkjcziybxowkaibqnndcjbsoehtamhspnidjylyisiaewmypfyiqtwlmejkpzlieolfdjnxntonnzfgcqlcfpoxcwqctalwrgwhvqvtrpwemxhirpgizjffqgntsmvzldpjfijdncexbwtxnmbnoykxshkqbounzrewkpqjxocvaufnhunsmsazgibxedtopnccriwcfzeomsrrangufkjfzipkmwfbmkarnyyrgdsooosgqlkzvorrrsaveuoxjeajvbdpgxlcrtqomliphnlehgrzgwujogxteyulphhuhwyoyvcxqatfkboahfqhjgujcaapoyqtsdqfwnijlkknuralezqmcryvkankszmzpgqutojoyzsnyfwsyeqqzrlhzbc"));
     }
 
-    public String longestPalindrome(String s) {
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        String result = "";
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i; j < n; j++) {
-                dp[i][j] = (s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]));
+//    public String longestPalindrome(String s) {
+//        int n = s.length();
+//        boolean[][] dp = new boolean[n][n];
+//        String result = "";
+//        for (int i = n - 1; i >= 0; i--) {
+//            for (int j = i; j < n; j++) {
+//                dp[i][j] = (s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]));
+//
+//                if (dp[i][j] && j - i + 1 > result.length()) {
+//                    result = s.substring(i, j + 1);
+//                }
+//            }
+//        }
+//        return result;
+//    }
 
-                if (dp[i][j] && j - i + 1 > result.length()) {
-                    result = s.substring(i, j + 1);
-                }
-            }
+    public String longestPalindrome(String s) {
+        String res = "";
+        for (int i = 0; i < s.length(); i++) {
+            String s1 = palindrome(s, i, i);
+            String s2 = palindrome(s, i, i + 1);
+            res = res.length() > s1.length() ? res : s1;
+            res = res.length() > s2.length() ? res : s2;
         }
-        return result;
+        return res;
+    }
+
+    private String palindrome(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
     }
 
 }

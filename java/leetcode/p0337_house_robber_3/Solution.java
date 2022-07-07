345c6672c6679ae912b083837e38b25a509c0d7a
atdt <jasond1016@gmail.com>
2022-07-07 19:49:13 +0800

    LeetCode 337. House Robber III(add solution)

--- java/leetcode/p0337_house_robber_3/Solution.java
+++ java/leetcode/p0337_house_robber_3/Solution.java
@@ -32,7 +32,7 @@ public class Solution {
                 new TreeNode(3,
                         null,
                         new TreeNode(1)));
-        System.out.println(solution.rob(root)); // 7
+        System.out.println(solution.rob2(root)); // 7
 
         root = new TreeNode(3,
                 new TreeNode(4,
@@ -41,10 +41,11 @@ public class Solution {
                 new TreeNode(5,
                         null,
                         new TreeNode(1)));
-        System.out.println(solution.rob(root)); // 9
+        System.out.println(solution.rob2(root)); // 9
     }
 
     private Map<TreeNode, Integer> memo = new HashMap<>();
+
     public int rob(TreeNode root) {
         if (root == null) {
             return 0;
@@ -62,6 +63,30 @@ public class Solution {
         int noRob = rob(root.left) + rob(root.right);
         int res = Math.max(rob, noRob);
         memo.put(root, res);
+        return res;
+    }
+
+    public int rob2(TreeNode root) {
+        int[] res = dp(root);
+        return Math.max(res[0], res[1]);
+    }
+
+    // 返回一个大小为 2 的数组
+    // arr[0] 代表不抢 root，能获得的最大钱数
+    // arr[1] 代表抢 root，能获得的最大钱数
+    private int[] dp(TreeNode root) {
+        if (root == null) {
+            return new int[]{0, 0};
+        }
+
+        int[] left = dp(root.left);
+        int[] right = dp(root.right);
+
+        int[] res = new int[2];
+        // 抢root，就不能抢左右子节点
+        res[1] = root.val + left[0] + right[0];
+        // 不抢root，可抢可不抢左右子节点
+        res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
         return res;
     }
 }

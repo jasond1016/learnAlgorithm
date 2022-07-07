cc14b8aa85e5c26837cf5a41f9a91e16b81a80ef
atdt <jasond1016@gmail.com>
2022-07-07 19:10:12 +0800

    LeetCode 198. House Robber(add solutions)

--- java/leetcode/p0198_house_robber/Solution.java
+++ java/leetcode/p0198_house_robber/Solution.java
@@ -6,10 +6,10 @@ public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] nums = new int[]{1,2,3,1};
-        System.out.println(solution.rob(nums));
+        System.out.println(solution.rob3(nums));
 
         nums = new int[]{2,7,9,3,1};
-        System.out.println(solution.rob(nums));
+        System.out.println(solution.rob3(nums));
     }
 
     // 备忘录
@@ -20,6 +20,7 @@ public class Solution {
         return dp(nums, 0);
     }
 
+    // 自顶向下
     private int dp(int[] nums, int start) {
         if (start >= nums.length) {
             return 0;
@@ -36,5 +37,31 @@ public class Solution {
                 nums[start] + dp(nums, start + 2));
         memo[start] = res;
         return res;
+    }
+
+    // 自底向上
+    public int rob2(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n + 2];
+
+        for (int i = n - 1; i >=0 ; i--) {
+            dp[i] = Math.max(dp[i + 1], nums[i] + dp[i + 2]);
+        }
+        return dp[0];
+    }
+
+    // 空间复杂度优化
+    public int rob3(int[] nums) {
+        int n = nums.length;
+        int dpi = 0;
+        int dp1 = 0;
+        int dp2 = 0;
+        
+        for (int i = n - 1; i >=0 ; i--) {
+            dpi = Math.max(dp1, nums[i] + dp2);
+            dp2 = dp1;
+            dp1 = dpi;
+        }
+        return dpi;
     }
 }

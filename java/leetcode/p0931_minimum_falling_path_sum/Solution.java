d969bf88df2e217020b1f17bcd8d8d6451a2ba03
atdt <jasond1016@gmail.com>
2022-08-08 19:19:56 +0800

    LeetCode 931. Minimum Falling Path Sum(add memo)

--- java/leetcode/p0931_minimum_falling_path_sum/Solution.java
+++ java/leetcode/p0931_minimum_falling_path_sum/Solution.java
@@ -1,5 +1,7 @@
 package leetcode.p0931_minimum_falling_path_sum;
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
@@ -17,8 +19,13 @@ public class Solution {
         System.out.println(solution.minFallingPathSum(matrix)); // -59
     }
 
+    private int[][] memo;
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
+        memo = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(memo[i], 66666);
+        }
         int res = Integer.MAX_VALUE;
         for (int i = 0; i < n; i++) {
             res = Math.min(res, dp(matrix, n - 1, i));
@@ -34,6 +41,10 @@ public class Solution {
             return matrix[i][j];
         }
 
-        return matrix[i][j] + Math.min(Math.min(dp(matrix, i - 1, j), dp(matrix, i - 1, j - 1)), dp(matrix, i - 1, j + 1));
+        if (memo[i][j] != 66666) {
+            return memo[i][j];
+        }
+        memo[i][j] = matrix[i][j] + Math.min(Math.min(dp(matrix, i - 1, j), dp(matrix, i - 1, j - 1)), dp(matrix, i - 1, j + 1));
+        return memo[i][j];
     }
 }
